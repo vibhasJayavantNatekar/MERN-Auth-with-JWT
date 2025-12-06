c2edfe7636765bb95853dc9962517c2a17cbd6c9
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-12-06 20:40:15 +0530

    Aplying validators on email and password

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,35 +1,46 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema(
     {
-        email:{
-            type:String,
-            required:true,
-            unique:true
+        email: {
+            type: String,
+            required: true,
+            unique: true
         },
-        password:{
-            type:String,
-            required:true
+        password: {
+            type: String,
+            required: true
         }
     }
 )
 
 //Static signup method
-userSchema.statics.signup = async function (email , password) { 
-   
-    const exists = await this.findOne({email})
+userSchema.statics.signup = async function (email, password){
+    //validation
+    if (!email || !password) {
+        throw Error('All field must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    // if (!validator.isStrongPassword(password)) {
+    //     throw Error('Password is not Strong')
+    // }
 
-    if(exists){
-         throw Error('Email already in use')
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
     }
 
     const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password,salt)
+    const hash = await bcrypt.hash(password, salt)
 
-    const user = await this.create({email , password: hash}) 
+    const user = await this.create({ email, password: hash })
 
     return user
 }
 
-module.exports = mongoose.model('user',userSchema)
+module.exports = mongoose.model('user', userSchema)
