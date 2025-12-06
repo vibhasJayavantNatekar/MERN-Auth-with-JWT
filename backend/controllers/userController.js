c04b9a97cc05ee4466c416af1a51efd179b76c6c
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-12-06 22:33:51 +0530

    Create a signing token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/usermodel')
+const jwt  = require('jsonwebtoken')
+
+const createToken = (_id)=>{
+ return  jwt.sign({_id },process.env.SECRET , {expiresIn:'3d'})
+}
 
 //loginuser
 const loginuser = async (req,res)=>{
@@ -12,8 +17,11 @@ const signupuser = async (req,res)=>{
 
     try {
         const user =await User.signup(email , password)
+         
+        //create a token
+       const token = createToken(user._id)
 
-        res.status(200).json({email ,user})
+        res.status(200).json({email ,token})
     } catch (err) {
         
         res.status(400).json({error : err.message})
