f06ca368e60d37b750f59ae931e62715382b83a0
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2025-12-07 14:32:31 +0530

    Assigning workouts to users

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,10 +1,14 @@
 
 import { useAuthContext } from './useAuthContext'
+import {useWorkoutsContext} from './useWorkoutsContext'
 import React from 'react'
+
 
 export const useLogout = () => {
 
     const {dispatch} = useAuthContext()
+    const {dispatch : workoutDispatch} = useWorkoutsContext()
+
 
  const logout =()=>{
     //remove user from storage
@@ -12,6 +16,8 @@ export const useLogout = () => {
 
     //dispatch logout action
     dispatch({type: 'LOGOUT'})
+    workoutDispatch({type: 'LOGOUT'})
+    workoutDispatch({type:'SET_WORKOUTS' ,payload:null})
 
  }
 
